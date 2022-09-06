374c223fdf4557b7bbb721209d60676cfc054989
gio2nice <106710271+gio2nice@users.noreply.github.com>
2022-09-05 22:16:12 -0700

    lil

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_DB || 'mongodb://localhost/socialnet', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socialnetwork', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
